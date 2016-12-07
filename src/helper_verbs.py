698a2f1b67772e17f777cf1d1bf5585083ab59f3
Joseph Daniel Winters <jdwinters96@gmail.com>
2016-12-07 14:35:20 -0500

    Added a constant set of DR MRS VANDERTRAMPP verbs

--- src/helper_verbs.py
+++ src/helper_verbs.py
@@ -8,9 +8,15 @@
 import conjugation as conj
 import tense as tn
 
+
 ALLER_PRESENT = conj.Conjugation("aller", tn.Tense.PRESENT,
                                  "vais", "vas", "va", "allons", "allez", "vont")
 AVOIR_PRESENT = conj.Conjugation("avoir", tn.Tense.PRESENT,
                                  "ai", "as", "a", "avons", "avez", "ont")
 ETRE_PRESENT = conj.Conjugation("être", tn.Tense.PRESENT,
                                 "suis", "es", "est", "sommes", "êtes", "sont")
+
+DR_MRS_VANDERTRAMPP_VERBS = {"devenir", "revenir", "monter", "rentrer",
+                             "sortir", "venir", "arriver", "naître",
+                             "descendre", "entrer", "retourner", "tomber",
+                             "rester", "aller", "mourir", "passer", "partir"}
